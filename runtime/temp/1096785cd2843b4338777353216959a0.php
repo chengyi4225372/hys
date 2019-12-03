d40a1b1e0055c8e05ca5805d364175a29395b2d1
cheng <732345907@qq.com>
2019-12-03 19:41:04 +0800

    add

--- runtime/temp/1096785cd2843b4338777353216959a0.php
+++ runtime/temp/1096785cd2843b4338777353216959a0.php
@@ -1,4 +1,4 @@
-<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:105:"C:\Users\Administrator\Desktop\phpEnv5.6.0-Green\www\hys\public/../application/home\view\index\index.html";i:1575372596;}*/ ?>
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:105:"C:\Users\Administrator\Desktop\phpEnv5.6.0-Green\www\hys\public/../application/home\view\index\index.html";i:1575373193;}*/ ?>
 <!DOCTYPE>
 <html lang="en">
 
@@ -389,10 +389,10 @@
           <ul>
             <li><a href="/">惠优税</a></li>
             <li><a href="<?php echo config('curl.hlg'); ?>">惠灵工</a></li>
-            <li><a href="<?php echo url('/home/many/index'); ?>">惠多薪</a></li>
+            <li><a href="<?php echo config('curl.hdx'); ?>">惠多薪</a></li>
             <li><a href="<?php echo config('curl.hzs'); ?>">惠找事</a></li>
-            <li><a href="<?php echo url('/home/business/index'); ?>">惠创业</a></li>
-            <li><a href="<?php echo url('/home/launch/index'); ?>">惠企动</a></li>
+            <li><a href="<?php echo config('curl.hcy'); ?>">惠创业</a></li>
+            <li><a href="<?php echo config('curl.hqd'); ?>">惠企动</a></li>
           </ul>
         </div>
       </div>
