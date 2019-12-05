505e158d7127fb0a7f4c01e2e1a5928c44ea8506
cheng <732345907@qq.com>
2019-12-05 11:48:46 +0800

    add

--- runtime/temp/241ffa330a2da239713bd83ad5e01e47.php
+++ runtime/temp/241ffa330a2da239713bd83ad5e01e47.php
@@ -1,4 +1,4 @@
-<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:108:"C:\Users\Administrator\Desktop\phpEnv5.6.0-Green\www\hys\public/../application/home\view\industry\index.html";i:1575449660;}*/ ?>
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:108:"C:\Users\Administrator\Desktop\phpEnv5.6.0-Green\www\hys\public/../application/home\view\industry\index.html";i:1575453741;}*/ ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,7 +17,6 @@
     <script src='/static/common/js/index.js'></script>
     <script src='/static/common/js/public.js'></script>
     <script src='/static/home/js/industry.js'></script>
-    <script src='/static/common/js/tool.js'></script>
 </head>
 
 <body>
