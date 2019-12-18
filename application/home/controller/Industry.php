8b0c999ee4cf0d77e2937d0af9907fa9475ae09c
cheng <732345907@qq.com>
2019-12-18 20:59:32 +0800

    add

--- application/home/controller/Industry.php
+++ application/home/controller/Industry.php
@@ -78,6 +78,12 @@ class Industry extends BaseController
             $id = input('get.mid','','int');
             $info = Taxationservice::instance()->Getidinfo($id);
             $this->assign('info',$info);
+            //上一篇
+            $top  = Taxationservice::instance()->gettop($id);
+            //下一篇
+            $next = Taxationservice::instance()->getnext($id);
+            $this->assign('top',$top);
+            $this->assign('next',$next);
             $this->assign('title','行业新闻资讯详情');
             return $this->fetch();
         }
