fdd865b9e6734ae06b15a0f46a5e4abbf2c559ab
cheng <732345907@qq.com>
2019-12-18 22:00:50 +0800

    add

--- application/extra/curl.php
+++ application/extra/curl.php
@@ -3,8 +3,6 @@ return [
 //    'hlg' => 'http://172.26.3.13:8009/index.php',//惠灵工
     'hlg' => 'http://hui0g.hui7y.com',//惠灵工
 
-    'hqy' => 'https://www.hui7y.com/',//惠企云
-
 //    'website' => 'http://172.26.3.13:8008/index.php',//官网
     'website' => 'https://www.hui7y.com',//官网
 
@@ -14,6 +12,4 @@ return [
 //    'hys' => 'http://172.26.3.13:8011/index.php',//惠优税
     'hys' => 'http://hui3y.hui7y.com',//惠优税
 
-    'redirect_url' => 'http://hlg_pub.hui7y.com/#',//登录重定向地址
-    'login_url' => 'http://hlg_pub.hui7y.com/#/login',//登录地址
 ];
